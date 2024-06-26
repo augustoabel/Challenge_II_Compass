1180d5ba0ba8bcbae67fbc836702bc0e1280a51e
Augusto Abel <uni.augusto.abel@gmail.com>
2024-06-26 16:03:01 -0300

    Alterações 2

--- streaming-compass/src/App.tsx
+++ streaming-compass/src/App.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";*/
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
-import SectionGrid from './components/SectionGrid';
+
 
 
 
