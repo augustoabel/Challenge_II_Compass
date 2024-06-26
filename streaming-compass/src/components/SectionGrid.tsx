9bfd6f651689177dbd3714bb82b2e93305e83cf3
Fernando Salvatico <fernandosalvatico934@gmail.com>
2024-06-25 23:44:04 -0300

    modificações de espaçamento no carrossel

--- streaming-compass/src/components/SectionGrid.tsx
+++ streaming-compass/src/components/SectionGrid.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../index.css'
 
 const SectionGrid = () => {
   const settings = {
@@ -11,8 +10,8 @@ const SectionGrid = () => {
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 1,
-    swipeToSlide: true,
-    arrows: true,
+    swipeToSlide: true, // Permite ao usuário deslizar para navegar
+    arrows: true, // Mostra setas de navegação
     responsive: [
       {
         breakpoint: 1024,
@@ -38,38 +37,38 @@ const SectionGrid = () => {
     <div className="bg-custom-neutral p-20 text-white">
       <p className="text-xl">Coleções de Halloween</p>
 
-      <Slider {...settings} className="mt-10 mx-4">
-        <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 1</div>
-        <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 2</div>
-        <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 3</div>
-        <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 4</div>
-        <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 5</div>
-        <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 6</div>
+      <Slider {...settings} className="mt-10">
+        <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 1</div>
+        <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral ">Slide 2</div>
+        <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral ">Slide 3</div>
+        <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 4</div>
+        <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 5</div>
+        <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 6</div>
       </Slider>
 
-      <div className="mt-10 gap-3">
+      <div className="mt-10">
         <p className="text-xl">Séries em Alta</p>
 
-        <Slider {...settings} className="mt-10">
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 1</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 2</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 3</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 4</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 5</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 6</div>
+        <Slider {...settings} className="mt-1">
+          <div className="bg-blue-400 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 1</div>
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 2</div>
+          <div className="bg-blue-800 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 3</div>
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 4</div>
+          <div className="bg-blue-300 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 5</div>
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 6</div>
         </Slider>
       </div>
 
       <div className="mt-10">
         <p className="text-xl">Filmes em Alta</p>
 
-        <Slider {...settings} className="mt-10">
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 1</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 2</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 3</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 4</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 5</div>
-          <div className="slider-item bg-blue-500 h-80 rounded-lg">Slide 6</div>
+        <Slider {...settings} className="mt-2">
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 1</div>
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 2</div>
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 3</div>
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 4</div>
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 5</div>
+          <div className="bg-blue-500 h-80 rounded-[15px] border-8 border-custom-neutral">Slide 6</div>
         </Slider>
       </div>
     </div>
